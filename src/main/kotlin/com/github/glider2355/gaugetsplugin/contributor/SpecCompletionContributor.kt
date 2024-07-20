3a6289fb6688ff0f8b29b48afc47bb1b8edf23b0
Muramatsu <62125008+Glider2355@users.noreply.github.com>
2024-07-21 04:41:26 +0900

    軽微な修正

--- src/main/kotlin/com/github/glider2355/gaugetsplugin/contributor/SpecCompletionContributor.kt
+++ src/main/kotlin/com/github/glider2355/gaugetsplugin/contributor/SpecCompletionContributor.kt
@@ -3,7 +3,6 @@ package com.github.glider2355.gaugetsplugin.contributor
 import com.github.glider2355.gaugetsplugin.language.SpecLanguage
 import com.intellij.codeInsight.completion.*
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.patterns.StandardPatterns
 
 class SpecCompletionContributor : CompletionContributor() {
     init {
