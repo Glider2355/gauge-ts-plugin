5342e3c0fa07d3fe4e402f0fc90e3b16db4efe02
r_muramatsu <r_muramatsu@codmon.co.jp>
2024-07-22 23:36:00 +0900

    デザインのfix

--- src/main/kotlin/com/github/glider2355/gaugetsplugin/setting/SettingsConfigurable.kt
+++ src/main/kotlin/com/github/glider2355/gaugetsplugin/setting/SettingsConfigurable.kt
@@ -3,23 +3,41 @@ package com.github.glider2355.gaugetsplugin.setting
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.components.service
+import com.intellij.ui.components.JBList
+import com.intellij.util.ui.JBUI
 import javax.swing.*
+import java.awt.BorderLayout
+import java.awt.Dimension
 
 class SettingsConfigurable(private val project: Project) : Configurable {
     private var mySettingsComponent: JPanel? = null
     private val directoryListModel = DefaultListModel<String>()
-    private val directoryList = JList(directoryListModel)
+    private val directoryList = JBList(directoryListModel)
     private val addButton = JButton("Add Directory")
     private val removeButton = JButton("Remove Selected Directory")
 
     override fun createComponent(): JComponent? {
         if (mySettingsComponent == null) {
-            mySettingsComponent = JPanel()
-            mySettingsComponent!!.layout = BoxLayout(mySettingsComponent, BoxLayout.Y_AXIS)
-            mySettingsComponent!!.add(JLabel("Search Directories:"))
-            mySettingsComponent!!.add(JScrollPane(directoryList))
-            mySettingsComponent!!.add(addButton)
-            mySettingsComponent!!.add(removeButton)
+            mySettingsComponent = JPanel(BorderLayout())
+            mySettingsComponent!!.border = JBUI.Borders.empty(10)
+
+            val listPanel = JPanel(BorderLayout())
+            listPanel.border = JBUI.Borders.empty(10, 0)
+            listPanel.add(JLabel("Gauge Step Directories:"), BorderLayout.NORTH)
+            directoryList.visibleRowCount = 10
+            listPanel.add(JScrollPane(directoryList), BorderLayout.CENTER)
+
+            val buttonPanel = JPanel()
+            buttonPanel.layout = BoxLayout(buttonPanel, BoxLayout.Y_AXIS)
+            buttonPanel.border = JBUI.Borders.empty(10, 0)
+            addButton.maximumSize = Dimension(Int.MAX_VALUE, addButton.preferredSize.height)
+            removeButton.maximumSize = Dimension(Int.MAX_VALUE, removeButton.preferredSize.height)
+            buttonPanel.add(addButton)
+            buttonPanel.add(Box.createRigidArea(Dimension(0, 5)))
+            buttonPanel.add(removeButton)
+
+            mySettingsComponent!!.add(listPanel, BorderLayout.CENTER)
+            mySettingsComponent!!.add(buttonPanel, BorderLayout.SOUTH)
 
             // ボタンのアクションリスナーを追加
             addButton.addActionListener {
@@ -55,7 +73,7 @@ class SettingsConfigurable(private val project: Project) : Configurable {
     }
 
     override fun getDisplayName(): String {
-        return "My Plugin Settings"
+        return "Gauge Ts Plugin Settings"
     }
 
     override fun reset() {
