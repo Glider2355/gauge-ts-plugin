c3d070dcf22da652c7d50b8bd86d4a3895934e3b
r_muramatsu <r_muramatsu@codmon.co.jp>
2024-07-27 15:42:41 +0900

    うまく動かないが一旦push

--- src/main/kotlin/gauge/contributor/SpecCompletionContributor.kt
+++ src/main/kotlin/gauge/contributor/SpecCompletionContributor.kt
@@ -1,14 +1,14 @@
 package gauge.contributor
 
-import gauge.language.SpecLanguage
 import com.intellij.codeInsight.completion.*
 import com.intellij.patterns.PlatformPatterns
+import com.thoughtworks.gauge.language.Specification
 
 class SpecCompletionContributor : CompletionContributor() {
     init {
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement().withLanguage(SpecLanguage.INSTANCE),
+            PlatformPatterns.psiElement().withLanguage(Specification.INSTANCE),
             SpecCompletionProvider()
         )
     }
