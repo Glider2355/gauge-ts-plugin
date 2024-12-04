55977073de10aee2626cd485caea95a485f40421
r_muramatsu <r_muramatsu@codmon.co.jp>
2024-12-05 01:09:42 +0900

    リファクタ

--- src/main/kotlin/gauge/GaugeConstants.kt
+++ src/main/kotlin/gauge/GaugeConstants.kt
@@ -1,35 +1,17 @@
 package gauge
 
 object GaugeConstants {
-    const val PLUGIN_ID: String = "com.github.Glider2355.gauge"
-
     const val GAUGE: String = "gauge"
-    const val SPEC_EXTENSION: String = "spec"
-    const val CONCEPT_EXTENSION: String = "cpt"
-    const val MANIFEST_FILE: String = "manifest.json"
     const val RUN: String = "run"
-    const val INSTALL: String = "install"
-    const val DOCS: String = "docs"
-    const val VERSION: String = "--version"
-    const val MACHINE_READABLE: String = "--machine-readable"
-    const val SIMPLE_CONSOLE: String = "--simple-console"
     const val TAGS: String = "--tags"
     const val PARALLEL: String = "--parallel"
     const val PARALLEL_NODES: String = "-n"
     const val TABLE_ROWS: String = "--table-rows"
-    const val DAEMON: String = "daemon"
-    const val VALIDATE: String = "validate"
-    const val FORMAT: String = "format"
     const val ENV_FLAG: String = "--env"
-    const val INIT_FLAG: String = "init"
-    const val FAILED: String = "--failed"
     const val GAUGE_HOME: String = "GAUGE_HOME"
     const val GAUGE_CUSTOM_CLASSPATH: String = "gauge_custom_classpath"
     const val SPEC_FILE_DELIMITER: String = "||"
     const val SPEC_FILE_DELIMITER_REGEX: String = "\\|\\|"
     private const val COLON = ":"
     const val SPEC_SCENARIO_DELIMITER: String = COLON
-    const val CLASSPATH_DELIMITER: String = COLON
-    const val HIDE_SUGGESTION: String = "--hide-suggestion"
-    const val MIN_GAUGE_VERSION: String = "0.9.0"
 }
--- src/main/kotlin/gauge/execution/runner/event/ExecutionResult.kt
+++ src/main/kotlin/gauge/execution/runner/event/ExecutionResult.kt
@@ -9,11 +9,34 @@ data class ExecutionResult(
     var afterHookFailure: ExecutionError? = null,
     var table: TableInfo? = null
 ) {
-    fun skipped(): Boolean {
-        return status.equals(ExecutionEvent.SKIP, ignoreCase = true)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ExecutionResult
+
+        if (time != other.time) return false
+        if (status != other.status) return false
+        if (out != other.out) return false
+        if (errors != null) {
+            if (other.errors == null) return false
+            if (!errors.contentEquals(other.errors)) return false
+        } else if (other.errors != null) return false
+        if (beforeHookFailure != other.beforeHookFailure) return false
+        if (afterHookFailure != other.afterHookFailure) return false
+        if (table != other.table) return false
+
+        return true
     }
 
-    fun failed(): Boolean {
-        return status.equals(ExecutionEvent.FAIL, ignoreCase = true)
+    override fun hashCode(): Int {
+        var result = time ?: 0
+        result = 31 * result + (status?.hashCode() ?: 0)
+        result = 31 * result + (out?.hashCode() ?: 0)
+        result = 31 * result + (errors?.contentHashCode() ?: 0)
+        result = 31 * result + (beforeHookFailure?.hashCode() ?: 0)
+        result = 31 * result + (afterHookFailure?.hashCode() ?: 0)
+        result = 31 * result + (table?.hashCode() ?: 0)
+        return result
     }
 }
