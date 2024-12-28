b3ad32994bb87df396f3fecb005debff4a22763e
r_muramatsu <r_muramatsu@codmon.co.jp>
2024-12-28 21:46:58 +0900

    test: refrectionの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,8 +43,10 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation(kotlin("reflect"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
     testImplementation("io.mockk:mockk:1.13.14")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
